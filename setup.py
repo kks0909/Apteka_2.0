dc9f750cf25ff27924d95ac43221cf89a7dc8934
Ksenia <kk0909@mail.ru>
2022-02-25 01:36:23 +0300

    Revert "Delete setup.py"
    This reverts commit 5555849ab4377976fa90aa66eee66ac6e0eeb07c.

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+	name='Apteka_1.0',
+	version='1.0',
+	packages=[''],
+	url='',
+	license='',
+	author='Ksenia',
+	author_email='kksenia0909@gmail.com',
+	description='Tg bot for searching.'
+)
